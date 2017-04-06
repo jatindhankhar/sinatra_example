177ffe70755a36c20c9736028eb7a16cb0611600
Jatin Dhankhar <dhankhar.jatin@gmail.com>
2017-04-06 16:09:46 +0530

    Add the JSON example

--- hello.rb
+++ hello.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
-
+require 'json'
+require 'byebug'
 ## Associates Root of the website with following code block when a get request is processed
 get '/' do
     "Hello World"
@@ -15,6 +16,12 @@ get '/send/:room/?' do
  "Sending message to room no #{params[:room]}"
 end
 
+## Params has a key named splat which holds all the * characters captured
 get '/mix/*/with/*/?' do
 "I have #{params[:splat]} and I am going to mix #{params[:splat].first} with #{params[:splat].last} "
+end
+
+get '/json' do 
+    content_type :json
+    ENV.to_h.to_json
 end
